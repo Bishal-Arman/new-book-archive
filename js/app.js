c22e0b73c8d1d8fbad3155e885919ae457d6c78c
Bishal43 <www.bishalarman43@gmail.com>
2021-09-18 01:01:28 +0600

    triger enter button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+// Get the input field
+var input = document.getElementById("search-field");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.key === 'Enter') {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("button-addon2").click();
+  }
+});
+
 const errorMessage=document.getElementById('error-message')
 //search area for input field
 const searchBook=()=>{
